cde38b7ef56330890f72b9fc76101567725b236e
Tomasz Fidos <fidostomasz@gmail.com>
2023-01-17 11:42:35 +0100

    Added red highlighting for worst employees.

--- 6-wyplaty-premie/script.js
+++ 6-wyplaty-premie/script.js
@@ -24,26 +24,42 @@ const getAndListTopPeformers = () => {
     if (document.getElementById('najlepsi-pracownicy').innerText !== '') {
         document.getElementById('najlepsi-pracownicy').innerText = '';
     }
+    const bestSpan = document.getElementById('najlepsi-pracownicy');
+    const bestList = sortEmployeesPerWorktime().slice(0, 3).map(x => x.employee).join(', ');
+    if (bestSpan.innerText === '') {
+        bestSpan.innerText = bestList;
+    }
+}
+
+const highlightLeastWorker = () => {
+    sortEmployeesPerWorktime().forEach(x => {
+        if (x.worktime < 100) {
+           document.getElementById(x.id).querySelector('.pracownik').style.backgroundColor = 'red';
+        } else if (document.getElementById(x.id).querySelector('.pracownik').style.backgroundColor === 'red') {
+            document.getElementById(x.id).querySelector('.pracownik').style.backgroundColor = 'white';
+        }
+    });
+}
+
+function sortEmployeesPerWorktime() {
     const employees = document.querySelectorAll('div');
     const worktimeAndEmployees = [];
     for (let i=0; i< employees.length; i++) {
         const element = employees[i];
         if (element.id.includes('pracownik')) {
             worktimeAndEmployees.push({
+                'id': element.id,
                 'employee': element.querySelector('.pracownik').innerText,
                 'worktime': parseInt(element.querySelector('.czas').value),
             });
         }
     }
     worktimeAndEmployees.sort((x, y) => y.worktime - x.worktime);
-    const bestSpan = document.getElementById('najlepsi-pracownicy');
-    const bestList = worktimeAndEmployees.slice(0, 3).map(x => x.employee).join(', ');
-    if (bestSpan.innerText === '') {
-        bestSpan.innerText = bestList;
-    }
+    return worktimeAndEmployees;
 }
 
 calculationButton.addEventListener('click', calculateSalariesAndBonuses);
+calculationButton.addEventListener('click', highlightLeastWorker);
 salaryFields.forEach(elem => {
     elem.addEventListener('DOMSubtreeModified', getAndListTopPeformers)
 });
