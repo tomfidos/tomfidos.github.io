2299a363f0828981577610a0f3c24dc6a8422f0f
Tomasz Fidos <fidostomasz@gmail.com>
2023-01-23 15:33:58 +0100

    Added event listening with loops.

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -39,7 +39,7 @@ class Calculator {
       if (screenText === '' || screenText === '0') {
         document.getElementById('screen').innerText = newNumber;
       } else {
-        document.getElementById('screen').innerText = screenText + newNumber
+        document.getElementById('screen').innerText = screenText + newNumber;
       }
       this.pickedNumber = parseFloat(document.getElementById('screen').innerText);
     }
@@ -81,40 +81,21 @@ class Calculator {
   const newCalculator = new Calculator();
   
   const deleteButton = document.getElementById('delete');
-  const percentButton = document.getElementById('percent');
-  const plusButton = document.getElementById('plus');
-  const sevenButton = document.getElementById('seven');
-  const eightButton = document.getElementById('eight');
-  const nineButton = document.getElementById('nine');
-  const minusButton = document.getElementById('minus');
-  const fourButton = document.getElementById('four');
-  const fiveButton = document.getElementById('five');
-  const sixButton = document.getElementById('six');
-  const multiplicationButton = document.getElementById('multi');
-  const oneButton = document.getElementById('one');
-  const twoButton = document.getElementById('two');
-  const threeButton = document.getElementById('three');
-  const divideButton = document.getElementById('division');
-  const zeroButton = document.getElementById('zero');
   const dotButton = document.getElementById('dot');
   const returnButton = document.getElementById('return');
   
   deleteButton.addEventListener('click', newCalculator.deleteNumber);
-  percentButton.addEventListener('click', newCalculator.changeSign);
-  plusButton.addEventListener('click', newCalculator.changeSign);
-  sevenButton.addEventListener('click', newCalculator.writeNumber);
-  eightButton.addEventListener('click', newCalculator.writeNumber);
-  nineButton.addEventListener('click', newCalculator.writeNumber);
-  minusButton.addEventListener('click', newCalculator.changeSign);
-  fourButton.addEventListener('click', newCalculator.writeNumber);
-  fiveButton.addEventListener('click', newCalculator.writeNumber);
-  sixButton.addEventListener('click', newCalculator.writeNumber);
-  multiplicationButton.addEventListener('click', newCalculator.changeSign);
-  oneButton.addEventListener('click', newCalculator.writeNumber);
-  twoButton.addEventListener('click', newCalculator.writeNumber);
-  threeButton.addEventListener('click', newCalculator.writeNumber);
-  divideButton.addEventListener('click', newCalculator.changeSign);
-  zeroButton.addEventListener('click', newCalculator.writeNumber);
   dotButton.addEventListener('click', newCalculator.writeDot);
   returnButton.addEventListener('click', newCalculator.returnResult);
+
+  document.querySelectorAll('.number').forEach(number => {
+    number.addEventListener('click', event => {
+      newCalculator.writeNumber(event);
+    });
+  });
+  document.querySelectorAll('.sign').forEach(sign => {
+    sign.addEventListener('click', event => {
+      newCalculator.changeSign(event);
+    });
+  });
   
